5977f6203c8ea01a8e02bbc3e6b0afb04b6638ba
Washington S Vieira <washingtonsampaio.23@gmail.com>
2023-05-18 19:23:47 -0300

    feat: Finalizando a primeira pagina do projeto web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,7 @@
+import { User } from 'lucide-react'
+import Image from 'next/image'
+import nlwLogo from '../assets/logo.svg'
+
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2 bg-[url(../assets/bg-stars.svg)] bg-cover">
@@ -8,6 +12,40 @@ export default function Home() {
 
         {/** Stripes */}
         <div className="border-r/10 absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+        <a href="" className="flex items-center gap-3 text-left">
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+          <p className="max-w-[140px] text-sm leading-snug transition-colors hover:text-gray-50">
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias!
+          </p>
+        </a>
+        {/* Hero */}
+        <div className="space-y-5">
+          <Image src={nlwLogo} alt="NLW Space Time"></Image>
+        </div>
+        <div className="max-w-[420px] space-y-5">
+          <h1 className="text-4xl font-bold leading-tight">
+            Sua Cápsula do tempo
+          </h1>
+          <p className="text-lg leading-relaxed">
+            Colecione momentos marcantes da sua jornada e compartilhe com o
+            mundo!
+          </p>
+          <a
+            href=""
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-700"
+          >
+            {' '}
+            CADASTRAR LEMBRANÇA{' '}
+          </a>
+        </div>
+
+        {/* Copyright */}
+        <div className="text-sm leading-relaxed text-gray-200">
+          Feito com para estudo por Washington S Vieira
+        </div>
       </div>
 
       {/** Right */}
